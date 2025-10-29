af96b3618f0d01e70073fcac0814fe4e460cbf74
Aveek <aveekpatra2004@gmail.com>
2025-10-30 00:44:24 +0100

    build(next): configure static export and image optimization
    Set output to 'export' for static site generation and disable image optimization to work with static exports

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'html.tailus.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
